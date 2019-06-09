fbd340a9b71e9613f49b1fc0f77e57a26053798d
yy1610918518 <1610918518@qq.com>
2019-06-09 21:22:50 +0800

    TaskDetailMapper接口

---
+++ src/main/java/org/hzero/hatc/infra/mapper/TaskDetailMapper.java
@@ -0,0 +1,12 @@
+package org.hzero.hatc.infra.mapper;
+
+import io.choerodon.mybatis.common.BaseMapper;
+import org.hzero.hatc.domain.entity.TaskDetail;
+
+/**
+ * TaskDetailMapper接口
+ * @author jiayanyan 2019/06/09
+ */
+public interface TaskDetailMapper extends BaseMapper<TaskDetail> {
+
+}
